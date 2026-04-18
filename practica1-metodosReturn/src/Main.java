d54d89a07592ef60f65edd24af8731039ecd4655
Alessandro Pillajo <alepillajof@gmail.com>
2026-04-18 12:08:14 -0500

    Calculo de promedio final

---
+++ practica1-metodosReturn/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        Calificacion c1 = new Calificacion();
+        c1.ingresarDatos();
+        c1.calcularPromedio();
+        c1.obtenerEstado();
+        c1.mostrarReporte();
+    }
+}
